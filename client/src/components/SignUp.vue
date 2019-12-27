8bb427dd230f2ecd585fb83818f4b873d5b8f480
TVM13 <71heal1bwi@hft-stuttgart.de>
2019-12-27 15:26:40 +0100

    SignUp Update

--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -9,13 +9,13 @@
       id="registrieren"
       tabindex="-1"
       role="dialog"
-      aria-labelledby="exampleModalLongTitle"
+      aria-labelledby="registrierenTitle"
       aria-hidden="true"
     >
       <div class="modal-dialog modal-dialog-scrollable" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Erzähle uns von dir</h5>
+            <h5 class="modal-title" id="registrierenTitle">Erzähle uns von dir</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
