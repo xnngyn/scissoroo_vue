98a845d3ea9eaafa5a223697480976d8e2afdcab
adriannahlik <adrian.nahlik@gmx.de>
2020-01-11 12:27:13 +0100

    Button Einfügung in eingeloggt

--- client/src/components/HomeEingeloggt.vue
+++ client/src/components/HomeEingeloggt.vue
@@ -3,11 +3,12 @@
 
 
 <h1> EINGELOGGT! </h1>
-        <button>Mein Profil</button> 
-        <button>Ausloggen</button>
-
-        
-
+      <!-- Button trigger modal -->
+    <button id="btnSignUp" class="btn btn-secondary mr-sm-2" data-toggle="modal" data-target="#registrieren">Registrierung</button>
+    
+    <!--  <button class="btn btn-secondary mr-sm-2" @click="setSignUp">Registrierung</button> -->
+    <button id="btnSignIn" class="btn btn-secondary" @click="setSignIn">Anmelden</button>
+    <button class="btn btn-secondary" @click="setResults">Results</button>
 
         
         
