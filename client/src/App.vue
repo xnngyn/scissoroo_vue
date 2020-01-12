55cae58050e64c1f00f7f5ba6c4063d05c4c086d
TVM13 <71heal1bwi@hft-stuttgart.de>
2020-01-12 22:16:54 +0100

    Style-Update

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,7 @@
 
     <nav class="navbar navbar-light bg-light">
 		  <a class="navbar-brand"><div @click="setSuchfeld(); setHomeText();"><img src="https://raw.githubusercontent.com/xnngyn/Scissoroo/master/public/images/Scissoroo_Logo.jpg" width="100" height="100" alt=""> </div></a> <!--../Bilder/Scissoroo_Logo.jpg-->
-			<button id="home" @click="setSuchfeld(); setHomeText();">Home</button>
+			<button id="home" class="btn btn-secondary" @click="setSuchfeld(); setHomeText();">Home</button>
       <ButtonsNichtEingeloggt v-if="!this.$store.state.eingeloggt"></ButtonsNichtEingeloggt>
       <ButtonsEingeloggt v-else-if="this.$store.state.eingeloggt"></ButtonsEingeloggt>
       
