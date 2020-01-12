84646bd84c2fffd2f17d00ef5378d2452c3883bd
TVM13 <71heal1bwi@hft-stuttgart.de>
2020-01-12 18:50:10 +0100

    Search Update

--- client/src/components/Suchfeld.vue
+++ client/src/components/Suchfeld.vue
@@ -1,64 +1,47 @@
 <template>
-    <div id ="suchfeld">
+  <div id="suchfeld" class="container fluid padding">
+    <!-- Load icon library -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
 
-
-
-
-     
-<!-- Load icon library -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-<!-- The form -->
-<form class="example" action="action_page.php">
-  <input type="text" placeholder="Search.." name="search">
-  <button type="submit" @click="setResults"><i class="fa fa-search"></i></button>
-</form>
-        
-
+    <div class="row justify-content-center text-center">
+      <div class="col-6">
+        <!-- The form -->
+        <form class="example" action="action_page.php">
+          <input type="text" placeholder="Search.." name="search" />
+          <button type="submit" @click="setResults">
+            <i class="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
-
-
-
-
-
 export default {
-
-    
-    name: 'suchfeld',
-    data () {
-        return {
-
-             
-            
-   
-    }
+  name: "suchfeld",
+  data() {
+    return {};
   },
 
   methods: {
-   
-      setResults: function() {
-
-          this.$store.state.componentStateEingeloggt = 'Results'
-      }
-    
-   
-
+    setResults: function() {
+      this.$store.state.componentStateEingeloggt = "Results";
+    }
   }
-}
+};
 </script>
 
 <style>
-
 * {
   box-sizing: border-box;
 }
 
 /* Style the search field */
-form.example input[type=text] {
+form.example input[type="text"] {
   padding: 10px;
   font-size: 17px;
   border: 1px solid grey;
@@ -72,7 +55,7 @@ form.example button {
   float: left;
   width: 20%;
   padding: 10px;
-  background: #2196F3;
+  background: #2196f3;
   color: white;
   font-size: 17px;
   border: 1px solid grey;
@@ -90,6 +73,4 @@ form.example::after {
   clear: both;
   display: table;
 }
- 
- 
 </style>
