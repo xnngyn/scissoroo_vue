5c962c454df1e064368ad60f70fc8773e3fb90b1
TVM13 <71heal1bwi@hft-stuttgart.de>
2020-01-12 17:52:41 +0100

    Update Buttons NichtEingeloggt

---
+++ client/src/components/ButtonsEingeloggt.vue
@@ -0,0 +1,64 @@
+<template>
+    <div id="eingeloggt">
+		<button id="btnMyProfil" class="btn btn-secondary mr-sm-2" @click="setMyProfile">Mein Profil</button>
+		<button id="btnLogout" class="btn btn-secondary">Ausloggen</button>
+
+
+
+
+       
+    
+        <component v-bind:is="$store.getters.componentStateEingeloggt"></component>
+         
+        
+
+    </div>
+
+</template>
+
+<script>
+
+import MyProfile from "./MyProfile";
+import Suchfeld from "./Suchfeld";
+import Results from "./Results";
+
+
+
+export default {
+
+    components: {
+        'MyProfile': MyProfile,
+        'Suchfeld': Suchfeld,
+        'Results': Results
+        
+    },
+    name: 'eingeloggt',
+    data () {
+        return {
+
+          ladeKomponente: 'Suchfeld',
+         
+            
+   
+    };
+  },
+  methods: {
+
+    setMyProfile: function() {
+      this.$store.state.componentStateEingeloggt = "MyProfile";
+    },
+
+    setResults: function() {
+        this.$store.state.componentStateEingeloggt = 'Results';
+      }
+
+  }
+}
+</script>
+
+<style>
+
+ 
+
+
+</style>
