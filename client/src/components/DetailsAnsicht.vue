d7afac90240170173caea134d2aa6fb044645120
Illya Ost <illyawade@gmail.com>
2020-01-13 01:25:18 +0100

    DetailsAnsicht

--- client/src/components/DetailsAnsicht.vue
+++ client/src/components/DetailsAnsicht.vue
@@ -35,7 +35,7 @@
 						
 					</div>
 					<div class="details col-md-6">
-						<h3 class="product-title">men's shoes fashion</h3>
+						<h3 class="product-title">Friseursalon Name</h3>
 						<div class="rating">
 							<div class="stars">
 								<span class="fa fa-star checked"></span>
@@ -46,23 +46,23 @@
 							</div>
 							<span class="review-no">41 reviews</span>
 						</div>
-						<p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>
+						<p class="product-description">Adresse, Telefon, Email, website</p>
 						<h4 class="price">current price: <span>$180</span></h4>
-						<p class="vote"><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></p>
-						<h5 class="sizes">sizes:
-							<span class="size" data-toggle="tooltip" title="small">s</span>
-							<span class="size" data-toggle="tooltip" title="medium">m</span>
-							<span class="size" data-toggle="tooltip" title="large">l</span>
-							<span class="size" data-toggle="tooltip" title="xtra large">xl</span>
+						<p class="vote"><strong>1</strong> Platz <strong>(87 votes)</strong></p>
+						<h5 class="sizes">Öffnungszeiten:
+							<!--<span class="size" data-toggle="tooltip" title="small">08:00 - 20:00</span>-->
+                            <span>08:00 - 20:00</span>
 						</h5>
-						<h5 class="colors">colors:
-							<span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
-							<span class="color green"></span>
-							<span class="color blue"></span>
+						<h5 class="colors">Mitarbeiter:
+							<!--<span class="size" data-toggle="tooltip" title="small">7</span>-->
+                            <span>7</span>
+						</h5>
+                        <h5 class="colors">Parkplätze:
+							<!--<span class="size" data-toggle="tooltip" title="small">7</span>-->
+                            <span>0</span>
 						</h5>
 						<div class="action">
-							<button class="add-to-cart btn btn-default" type="button">add to cart</button>
-							<button class="like btn btn-default" type="button"><span class="fa fa-heart"></span></button>
+							<button class="add-to-cart btn btn-default" type="button">Termin buchen</button>
 						</div>
 					</div>
 				</div>
