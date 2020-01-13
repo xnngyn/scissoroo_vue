10b1b3889fddb440c0968d44ef05992499edd08a
adriannahlik <adrian.nahlik@gmx.de>
2020-01-13 01:10:43 +0100

    änderung results

--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -9,15 +9,9 @@
             <h3><a  title="">{{ friseur.Name }}</a></h3>
 			<img src="" alt="" />
 			<p>{{ friseur.Adresse }}</p>
-			<p>{{ friseur.Email}}</p>
 			<p>{{ friseur.Telefon}}</p>
-			<p v-show="friseur.show">Montag: {{ friseur.Montag}}</p>
-			<p v-show="friseur.show">Dienstag: {{ friseur.Dienstag}}</p>
-			<p v-show="friseur.show">Mittwoch: {{ friseur.Mittwoch}}</p>
-			<p v-show="friseur.show">Donnerstag: {{ friseur.Donnerstag}}</p>
-			<p v-show="friseur.show">Freitag: {{ friseur.Freitag}}</p>
-			<p v-show="friseur.show">Samstag: {{ friseur.Samstag}}</p>
-			<p v-show="friseur.show">Sonntag: {{ friseur.Sonntag}}</p>
+            <p v-show="friseur.show">{{ friseur.Email}}</p>
+			<p v-show="friseur.show">{{ friseur.Website}}</p>
             <hr class="bg-white">
         </article>
         </div>
